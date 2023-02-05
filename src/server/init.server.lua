d03b1f8e33182f9df36e4f0bdb65b397477b2ee8
mattyxcx <mattwyc.roblox@gmail.com>
2023-02-05 17:16:14 +0000

    Update init.server.lua

--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -11,7 +11,18 @@ local onInvoke = function(plr,header,args)
     elseif header == "search" then
         return search.queue(plr,unpack(args))
     elseif header == "apply" then
-        
+        local loc,id = unpack(args)
+        if dir.ct_fld.Items:FindFirstChild(loc) then
+            if dir.ct_fld.Items[loc]:FindFirstChild(id) == nil then
+                local item = unpack(sv.insertService:LoadAsset(id):GetChildren())
+                item.Name = id
+                item.Parent = dir.ct_fld.Items[loc]
+            end
+            local item = dir.ct_fld.Items[loc][id]
+            if plr.Character:FindFirstChild(item.Name) then return { 2, "I already equipped this" } end
+            item:Clone().Parent = plr.Character
+            return { 1 }
+        else return { 2, "Unknown category" } end
     end
 end
 
@@ -24,7 +35,7 @@ local setup = function()
             if resp[1] == 1 then
                 all[v] = resp
                 succ = true
-                task.wait(0.12)
+                task.wait(0.2)
             else
                 task.wait(5)
                 resp = search.queue("server",v,"","")
